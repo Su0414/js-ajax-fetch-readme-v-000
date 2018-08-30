96155286debe261483a8aaac4e6c55f49d044cd2
Suchitra Naik <susanaik@gmail.com>
2018-08-30 16:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const app = "I don't do much.";
 
-fetch('https://api.github.com/user/repos').
-  then(res => res.json()).
-  then(json => console.log(json))
-
-  const token = 'YOUR_TOKEN_HERE'
+const token = 'YOUR_TOKEN_HERE'
   fetch('https://api.github.com/user/repos', {
     headers: {
       Authorization: `token ${token}`
